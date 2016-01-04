4a051ea1ee48c044a553598c38cdc27ba3586dde
川沼大輝 <daiki.kawanuma@gmail.com>
2016-01-04 19:53:52 +0900

    implement SemanticDetailView

--- ECOLOGSemanticViewer/ViewModels/WindowViewModels/SemanticDetailWindowViewModel.cs
+++ ECOLOGSemanticViewer/ViewModels/WindowViewModels/SemanticDetailWindowViewModel.cs
@@ -12,12 +12,35 @@ using Livet.EventListeners;
 using Livet.Messaging.Windows;
 
 using ECOLOGSemanticViewer.Models;
+using ECOLOGSemanticViewer.Views.Pages;
+using System.Windows.Controls;
 
 namespace ECOLOGSemanticViewer.ViewModels.WindowViewModels
 {
     public class SemanticDetailWindowViewModel : ViewModel
     {
-        
+
+        #region CurrentPage変更通知プロパティ
+        private Page _CurrentPage;
+
+        public Page CurrentPage
+        {
+            get
+            { return _CurrentPage; }
+            set
+            { 
+                if (_CurrentPage == value)
+                    return;
+                _CurrentPage = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        public SemanticDetailWindowViewModel()
+        {
+            this.CurrentPage = new DetailEnergyPage();
+        }
 
         public void Initialize()
         {
