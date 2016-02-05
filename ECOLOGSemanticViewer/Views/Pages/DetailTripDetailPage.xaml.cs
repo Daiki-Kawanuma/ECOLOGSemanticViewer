6c41959e88fbfe92161c913151dfa7a2e8792ff2
川沼大輝 <daiki.kawanuma@gmail.com>
2016-02-05 16:13:17 +0900

    implement mouse wheel

--- ECOLOGSemanticViewer/Views/Pages/DetailTripDetailPage.xaml.cs
+++ ECOLOGSemanticViewer/Views/Pages/DetailTripDetailPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECOLOGSemanticViewer.ViewModels.PageViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,38 @@ namespace ECOLOGSemanticViewer.Views.Pages
         public void InvokeScript(string scriptName, params object[] args)
         {
             this.webBrowser.InvokeScript(scriptName, args);
+        }
+
+        private void Page_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+            var context = this.DataContext as DetailTripDetailPageViewModel;
+
+            if (context == null)
+                return;
+
+            if (e.Key == Key.Right)
+            {
+                if (context.CurrentIndex < context.SliderMaximum)
+                    context.CurrentIndex++;
+            }
+            else
+            {
+                if (context.CurrentIndex > 0)
+                    context.CurrentIndex--;
+            }
+        }
+
+        private void Page_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var context = this.DataContext as DetailTripDetailPageViewModel;
+
+            if (context == null)
+                return;
+
+            if (context.CurrentIndex + e.Delta / 10 < context.SliderMaximum && context.CurrentIndex + e.Delta / 10 > 0)
+                context.CurrentIndex += e.Delta / 10;
+            // TODO アルゴリズムを考えて☆
         }
     }
 }
